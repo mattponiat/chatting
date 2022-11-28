b25349ab704d2c40682f9e8026dc33b10e07042c
Mateusz Poniatowski <pinkyball50@gmail.com>
2022-11-28 16:47:53 +0100

    feat: :sparkles: Add color picker slider

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,33 +1,89 @@
-//Backend
-import { useSession } from "next-auth/react";
-import { trpc } from "../utils/trpc";
+import * as React from "react";
+//Color wheel
+import { useColorSlider } from "@react-aria/color";
+import { type Color, useColorSliderState } from "@react-stately/color";
+import { VisuallyHidden } from "@react-aria/visually-hidden";
+import { useLocale } from "@react-aria/i18n";
+import { useFocusRing } from "@react-aria/focus";
 
-const ColorPicker = () => {
-  const changeColor = trpc.user.changeColor.useMutation();
-  const session = useSession();
+const TRACK_THICKNESS = 28;
+const THUMB_SIZE = 20;
 
-  const handleChangeColor = (color: string) => {
-    if (session.status === "authenticated") {
-      changeColor.mutateAsync({
-        color: color,
-      });
-    }
-  };
+type ColorPickerProps = {
+  channel:
+    | "hue"
+    | "saturation"
+    | "brightness"
+    | "lightness"
+    | "red"
+    | "green"
+    | "blue"
+    | "alpha";
+  defaultValue: string;
+  onChangeEnd?: (value: Color) => void;
+  onChange: React.Dispatch<React.SetStateAction<Color>>;
+  label?: string;
+};
+
+const ColorPicker = (props: ColorPickerProps) => {
+  const { locale } = useLocale();
+  const state = useColorSliderState({ ...props, locale });
+  const trackRef = React.useRef(null);
+  const inputRef = React.useRef(null);
+
+  // Default label to the channel name in the current locale
+  const label =
+    props.label || state.value.getChannelName(props.channel, locale);
+
+  const { trackProps, thumbProps, inputProps, labelProps, outputProps } =
+    useColorSlider(
+      {
+        ...props,
+        label,
+        trackRef,
+        inputRef,
+      },
+      state
+    );
+
+  const { focusProps, isFocusVisible } = useFocusRing();
 
   return (
-    <div className="mr-auto flex gap-2">
-      <button
-        className="bg-blue-500 p-4"
-        onClick={() => handleChangeColor("#3b82f6")}
-      />
-      <button
-        className="bg-red-500 p-4"
-        onClick={() => handleChangeColor("#ef4444")}
-      />
-      <button
-        className="bg-green-500 p-4"
-        onClick={() => handleChangeColor("#22c55e")}
-      />
+    <div className="flex w-full flex-col items-center">
+      <div className="flex self-stretch">
+        <label {...labelProps}>{label}</label>
+        <output
+          {...outputProps}
+          className="flex-shrink-0 flex-grow basis-auto text-end"
+        >
+          {state.value.formatChannelValue(props.channel, locale)}
+        </output>
+      </div>
+      <div
+        {...trackProps}
+        ref={trackRef}
+        style={{
+          ...trackProps.style,
+          height: TRACK_THICKNESS,
+        }}
+        className="w-full rounded-lg"
+      >
+        <div
+          {...thumbProps}
+          style={{
+            ...thumbProps.style,
+            top: TRACK_THICKNESS / 2,
+            width: isFocusVisible ? TRACK_THICKNESS + 4 : THUMB_SIZE,
+            height: isFocusVisible ? TRACK_THICKNESS + 4 : THUMB_SIZE,
+            background: state.getDisplayColor().toString("css"),
+          }}
+          className="box-border rounded-full border-2 border-black shadow-[0_0_0_0.5px_black]"
+        >
+          <VisuallyHidden>
+            <input ref={inputRef} {...inputProps} {...focusProps} />
+          </VisuallyHidden>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/TopPanel.tsx
+++ src/components/TopPanel.tsx
@@ -1,8 +1,12 @@
+import React from "react";
 //Backend
 import { trpc } from "../utils/trpc";
 import { signIn, useSession, signOut } from "next-auth/react";
 //Components
 import ColorPicker from "../components/ColorPicker";
+import { parseColor } from "@react-stately/color";
+//Utils
+import hexToCssHsl from "../utils/hexToHSL";
 
 type NullUser = {
   nullUser: {
@@ -15,12 +19,22 @@ type NullUser = {
 const TopPanel = ({ nullUser }: NullUser) => {
   const session = useSession();
   const currentUser = trpc.user.getCurrent.useQuery();
+  const changeColor = trpc.user.changeColor.useMutation();
+  const [color, setColor] = React.useState(parseColor("hsl(0, 100%, 50%)"));
+
+  const handleChangeColor = () => {
+    if (session.status === "authenticated") {
+      changeColor.mutateAsync({
+        color: color.toString("hex"),
+      });
+    }
+  };
 
   return (
     <div className="flex w-full max-w-3xl items-center justify-end gap-2 text-white">
       {session.status === "unauthenticated" || session.status === "loading" ? (
         <button
-          className="min-h-[40px] min-w-[80px] rounded-lg bg-neutral outline outline-1 outline-gray-600 transition-colors duration-150 hover:bg-base-100"
+          className="btn rounded-lg outline outline-1 outline-gray-600 transition-colors duration-300"
           onClick={() => signIn()}
         >
           Log In
@@ -40,9 +54,35 @@ const TopPanel = ({ nullUser }: NullUser) => {
               {session.data?.user?.name}
             </span>
           </div>
-          <ColorPicker />
+          {currentUser.data?.color ? (
+            <>
+              <label
+                htmlFor="my-modal-4"
+                className="btn mr-auto rounded-lg outline outline-1 outline-gray-600 transition-colors duration-300"
+              >
+                Change color
+              </label>
+              <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+              <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                <label className="modal-box relative" htmlFor="">
+                  <h3 className="mb-6 text-xl font-bold text-neutral-content">
+                    Pick a color for your chat&apos;s username
+                  </h3>
+                  <ColorPicker
+                    channel="hue"
+                    label="Hue"
+                    defaultValue={hexToCssHsl(
+                      currentUser.data?.color ?? "#FF0000"
+                    )}
+                    onChange={setColor}
+                    onChangeEnd={handleChangeColor}
+                  />
+                </label>
+              </label>
+            </>
+          ) : null}
           <button
-            className="min-h-[40px] min-w-[80px] rounded-lg bg-neutral outline outline-1 outline-gray-600 transition-colors duration-150 hover:bg-base-100"
+            className="btn rounded-lg outline outline-1 outline-gray-600 transition-colors duration-300"
             onClick={() => signOut()}
           >
             Log Out
