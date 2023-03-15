292dd9378562e8887856a4be4fc443b154638d77
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-03-15 20:13:36 +0100

    refactor: :recycle: Add channelId in router input

--- src/server/trpc/router/message.ts
+++ src/server/trpc/router/message.ts
@@ -11,6 +11,7 @@ export const messageRouter = router({
           .trim()
           .min(1, "Message can't be empty")
           .max(500, "Message can't be longer than 500 characters"),
+        channelId: z.string(),
       })
     )
     .mutation(async ({ input, ctx }) => {
@@ -18,18 +19,28 @@ export const messageRouter = router({
         data: {
           content: input.text,
           authorId: ctx.session?.user?.id,
+          channelId: input.channelId,
         },
         include: {
           author: true,
         },
       });
-      await pusher.trigger("chat", "message", message);
+      await pusher.trigger(input.channelId, "message", message);
 
       return message;
     }),
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.prisma.message.findMany({
-      include: { author: true },
-    });
-  }),
+  getAll: publicProcedure
+    .input(
+      z.object({
+        channelId: z.string(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const allMessages = await ctx.prisma.message.findMany({
+        where: { channelId: input.channelId },
+        include: { author: true },
+      });
+
+      return allMessages;
+    }),
 });
