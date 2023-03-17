d5568e315488e23db5b8272d920a3b7dc4ef38e3
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-03-17 19:00:36 +0100

    feat: :sparkles: Add functionality for making own channels

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
-//Backend
-import { trpc } from "src/utils/trpc";
 //Components
 import TopPanel from "src/components/TopPanel";
+import CreateChannelPanel from "src/components/CreateChannelPanel";
 
 const Home: NextPage = () => {
-  const router = useRouter();
-  const createChannel = trpc.channel.create.useMutation();
-
-  const handleCreateChannel = async () => {
-    const channel = await createChannel.mutateAsync();
-    const channelUrl = `/channel/${channel.id}`;
-
-    router.push(channelUrl);
-
-    return channel;
-  };
-
   return (
     <>
       <Head>
@@ -34,13 +20,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex h-[100dvh] max-w-full flex-col items-center p-6">
         <TopPanel />
-        <button
-          className="btn-secondary btn-lg btn my-auto"
-          type="button"
-          onClick={handleCreateChannel}
-        >
-          Create new channel
-        </button>
+        <CreateChannelPanel />
       </main>
     </>
   );
--- src/server/trpc/router/channel.ts
+++ src/server/trpc/router/channel.ts
@@ -1,11 +1,28 @@
 import { publicProcedure, router } from "src/server/trpc/trpc";
+import { z } from "zod";
 
 export const channelRouter = router({
-  create: publicProcedure.mutation(async ({ ctx }) => {
-    const channel = await ctx.prisma.channel.create({
+  create: publicProcedure
+    .input(z.object({ channelName: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const channel = await ctx.prisma.channel.create({
+        data: {
+          id: input.channelName,
+        },
+      });
+
+      return channel;
+    }),
+  createRandom: publicProcedure.mutation(async ({ ctx }) => {
+    const randomChannel = await ctx.prisma.channel.create({
       data: {},
     });
 
-    return channel;
+    return randomChannel;
+  }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const allChannels = await ctx.prisma.channel.findMany({});
+
+    return allChannels;
   }),
 });
