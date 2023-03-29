af97515e5f49349809cb80a9d40270142a9ec802
Mateusz Poniatowski <pinkyball50@gmail.com>
2023-03-29 02:36:59 +0200

    refactor: :recycle: Use getStaticProps to get channel id before hydration

--- src/pages/channel/[channelId].tsx
+++ src/pages/channel/[channelId].tsx
@@ -1,13 +1,16 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import * as React from "react";
 import { flushSync } from "react-dom";
-import { useRouter } from "next/router";
 //Backend
 import type { Message, User } from "@prisma/client";
 import { trpc } from "src/utils/trpc";
 import Pusher from "pusher-js";
 import { env } from "src/env/client.mjs";
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { appRouter } from "src/server/trpc/router/_app";
+import { prisma } from "src/server/db/client";
+import superjson from "superjson";
 //Components
 import TopPanel from "src/components/TopPanel";
 import InputPanel from "src/components/InputPanel";
@@ -18,14 +21,7 @@ import useChattingStore from "src/store/chattingStore";
 
 let loaded = false;
 
-const ChannelPage: NextPage = () => {
-  const { query, isReady } = useRouter();
-  const channelId = React.useMemo(() => {
-    if (isReady) {
-      return query.channelId as string;
-    } else return "";
-  }, [isReady, query.channelId]);
-
+const ChannelPage: NextPage<{ channelId: string }> = ({ channelId }) => {
   const { data: oldMessages, status } = trpc.message.getAll.useQuery(
     { channelId: channelId },
     { enabled: !loaded }
@@ -37,10 +33,9 @@ const ChannelPage: NextPage = () => {
   >([]);
   const [errorCode, setErrorCode] = React.useState(0);
   const { listRef } = useChattingStore((state) => ({ listRef: state.listRef }));
-  const allChannels = trpc.channel.getAll.useQuery();
-  const existingChannel = allChannels.data?.find(
-    (channel) => channel.id === channelId
-  );
+  const { data: existingChannel } = trpc.channel.getChannelById.useQuery({
+    channelId,
+  });
 
   const scrollToLastMessage = React.useCallback(() => {
     if (listRef.current != null) {
@@ -59,39 +54,37 @@ const ChannelPage: NextPage = () => {
       cluster: env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
     });
 
-    if (isReady) {
-      const channel = pusher.subscribe(channelId);
+    const channel = pusher.subscribe(channelId);
 
-      channel.bind(
-        "message",
-        (
-          message: Message & {
-            author: User;
-          }
-        ) => {
-          flushSync(() => {
-            message.timestamp = new Date(message.timestamp);
-            setNewMessages((newMessages) => [...newMessages, message]);
-          });
-          scrollToLastMessage();
+    channel.bind(
+      "message",
+      (
+        message: Message & {
+          author: User;
         }
-      );
+      ) => {
+        flushSync(() => {
+          message.timestamp = new Date(message.timestamp);
+          setNewMessages((newMessages) => [...newMessages, message]);
+        });
+        scrollToLastMessage();
+      }
+    );
 
-      loaded = true;
+    loaded = true;
 
-      return () => {
-        pusher.unsubscribe(channelId);
-      };
-    }
-  }, [channelId, scrollToLastMessage, isReady]);
+    return () => {
+      pusher.unsubscribe(channelId);
+    };
+  }, [channelId, scrollToLastMessage]);
 
   const messages = (oldMessages ?? []).concat(newMessages);
 
   React.useEffect(() => {
-    if (status === "success" && existingChannel === undefined) {
+    if (status === "success" && existingChannel?.id === undefined) {
       setErrorCode(404);
     }
-  }, [existingChannel, status]);
+  }, [existingChannel?.id, status]);
 
   return (
     <>
@@ -99,11 +92,11 @@ const ChannelPage: NextPage = () => {
         <meta
           name="description"
           property="og:description"
-          content="Chatting channel"
+          content={`Chatting Channel ${channelId}`}
         />
         <meta property="og:title" content="Chatting channel" />
         <meta property="og:url" content="https://chat.mattponiat.pl/" />
-        <title>Chatting channel</title>
+        <title>{channelId}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-[100dvh] max-w-full flex-col items-center gap-5 p-6">
@@ -119,6 +112,31 @@ const ChannelPage: NextPage = () => {
       </main>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: { prisma, session: null },
+    transformer: superjson,
+  });
+
+  const channelId = context.params?.channelId;
+
+  if (typeof channelId !== "string") throw new Error("No channel id");
+
+  await ssg.channel.getChannelById.prefetch({ channelId: channelId });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      channelId,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default ChannelPage;
